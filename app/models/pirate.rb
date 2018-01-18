8a6d0b01df549259eb493473bacc5a6d04d1f8de
iwilliams83 <indirajamurath@gmail.com>
2018-01-18 22:47:38 +0000

    Done.

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,25 @@
 class Pirate
+
+  attr_accessor :name, :weight, :height
+
+  @@pirates = []
+
+  def initialize
+    @name = name
+    @weight = weight
+    @height = height
+    @@pirates << self
+  end
+
+  def self.all
+    @@pirates
+  end
+
+  def self.clear
+    @@pirates.clear
+  end
+
+  def self.find(name)
+    @@pirates.select {|e| e.name == name}.first
+  end
 end
